ad8d05d2a7cfe08ea2056c1d5c597af86e9a2017
Andrew Mokotoff <andrewmokotoff@Andrews-MacBook-Pro.local>
2023-11-26 22:31:16 -0500

    final commit

---
+++ source/ui-chat/src/components/ChatMessage.tsx
@@ -0,0 +1,128 @@
+/**********************************************************************************************************************
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                                                *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+import { FC, memo, useContext, useState } from 'react';
+import { Button, Icon } from '@cloudscape-design/components';
+import HomeContext from '../home/home.context';
+import { Message } from '../types/chat';
+import { MemoizedReactMarkdown } from './MemoizedReactMarkdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import { CodeBlock } from './CodeBlock';
+import { Components } from 'react-markdown';
+
+export interface Props {
+    message: Message;
+    messageIndex: number;
+}
+
+const MARKDOWN_COMPONENTS: Components = {
+    code: ({ node, className, children, ...props }) => {
+        const match = /language-(\w+)/.exec(className || '');
+
+        return match ? (
+            <CodeBlock language={(match && match[1]) || ''} value={String(children).replace(/\n$/, '')} {...props} />
+        ) : (
+            <code className={className} {...props}>
+                {children}
+            </code>
+        );
+    },
+    table: ({ children }) => {
+        return <table>{children}</table>;
+    },
+    th: ({ children }) => {
+        return <th className="break-words border border-black bg-gray-500 px-3 py-1 text-white">{children}</th>;
+    },
+    td: ({ children }) => {
+        return <td className="break-words border border-black px-3 py-1">{children}</td>;
+    }
+};
+
+export const ChatMessage: FC<Props> = memo(({ message, messageIndex }) => {
+    const {
+        state: { selectedConversation, messageIsStreaming }
+    } = useContext(HomeContext);
+
+    const [messagedCopied, setMessageCopied] = useState(false);
+
+    const copyOnClick = () => {
+        if (!navigator.clipboard) return;
+
+        navigator.clipboard.writeText(message.content).then(() => {
+            setMessageCopied(true);
+            setTimeout(() => {
+                setMessageCopied(false);
+            }, 2000);
+        });
+    };
+
+    return (
+        <div className={`${message.role === 'assistant' ? 'bg-gray-100' : ''}`} style={{ overflowWrap: 'anywhere' }}>
+            <div className=" m-auto flex md:gap-6 md:py-6 xl:max-w-2xl">
+                {message.role === 'assistant' && (
+                    <div className="text-right font-bold ">
+                        <Icon name={'contact'} />
+                    </div>
+                )}
+
+                <div className="prose mt-[-2px] w-full">
+                    {message.role === 'user' ? (
+                        <div className="w-full">
+                            <div>{message.content}</div>
+                        </div>
+                    ) : (
+                        <div className="flex ">
+                            <div className="flex-1">
+                                <MemoizedReactMarkdown
+                                    className="prose dark:prose-invert flex-1"
+                                    remarkPlugins={[remarkGfm, remarkMath]}
+                                    disallowedElements={['a']}
+                                    unwrapDisallowed
+                                    components={MARKDOWN_COMPONENTS}
+                                >
+                                    {`${message.content}${
+                                        messageIsStreaming &&
+                                        messageIndex === (selectedConversation?.messages.length ?? 0) - 1
+                                            ? '▍'
+                                            : ''
+                                    }`}
+                                </MemoizedReactMarkdown>
+                            </div>
+                            <div className="copy-button-div md:-mr-8">
+                                {messagedCopied ? (
+                                    <Icon name="status-positive" />
+                                ) : (
+                                    <div className="copy-button-div-nested">
+                                        <Button
+                                            data-testid="copy-msg-button"
+                                            iconName="copy"
+                                            variant="icon"
+                                            onClick={copyOnClick}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
+                {message.role === 'user' && (
+                    <div className="min-w-[40px] text-right">
+                        <Icon name={'user-profile'} />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+});
+ChatMessage.displayName = 'ChatMessage';
